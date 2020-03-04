cb6385f3c95bf6088a460f7cf26e928833a2d0d0
Steven Herbst <sherbst@stanford.edu>
2020-03-04 15:44:15 -0800

    basic code works

--- tests/test_tap.py
+++ tests/test_tap.py
@@ -1,32 +1,33 @@
-import fault
 import magma as m
 from pathlib import Path
 from tapdance.jtag_drv import JTAGLowLevelDriver
 
 def test_tap(width=5):
     # declare circuit
-    dut = m.DeclareCircuit(
-        'DW_tap',
-        # inputs
-        'tck', m.BitIn,
-        'trst_n', m.BitIn,
-        'tms', m.BitIn,
-        'tdi', m.BitIn,
-        'so', m.BitIn,
-        'bypass_sel', m.BitIn,
-        'sentinel_val', m.In(m.Bits[width-1]),
-        'test', m.BitIn,
-        # outputs
-        'clock_dr', m.BitOut,
-        'shift_dr', m.BitOut,
-        'update_dr', m.BitOut,
-        'tdo', m.BitOut,
-        'tdo_en', m.BitOut,
-        'tap_state', m.Out(m.Bits[16]),
-        'extest', m.BitOut,
-        'samp_load', m.BitOut,
-        'instructions', m.Out(m.Bits[width])
-    )
+    class DW_tap(m.Circuit):
+        io = m.IO(
+            # inputs
+            tck=m.BitIn,
+            trst_n=m.BitIn,
+            tms=m.BitIn,
+            tdi=m.BitIn,
+            so=m.BitIn,
+            bypass_sel=m.BitIn,
+            sentinel_val=m.In(m.Bits[width-1]),
+            test=m.BitIn,
+            # outputs
+            clock_dr=m.BitOut,
+            shift_dr=m.BitOut,
+            update_dr=m.BitOut,
+            tdo=m.BitOut,
+            tdo_en=m.BitOut,
+            tap_state=m.Out(m.Bits[16]),
+            extest=m.BitOut,
+            samp_load=m.BitOut,
+            instructions=m.Out(m.Bits[width])
+        )
+
+    # specify parameters of the circuit
     parameters = {
         'width': width,
         'id': 1,
@@ -37,13 +38,13 @@ def test_tap(width=5):
     }
 
     # instantiate tester
-    t = JTAGLowLevelDriver(dut, width=width)
+    t = JTAGLowLevelDriver(DW_tap, width=width)
 
     # initialize custom signals
-    t.poke(dut.so, 0)
-    t.poke(dut.bypass_sel, 0)
-    t.poke(dut.sentinel_val, 0)
-    t.poke(dut.test, 0)
+    t.poke(DW_tap.so, 0)
+    t.poke(DW_tap.bypass_sel, 0)
+    t.poke(DW_tap.sentinel_val, 0)
+    t.poke(DW_tap.test, 0)
     t.eval()
 
     # reset the driver
@@ -53,15 +54,17 @@ def test_tap(width=5):
     id_val = t.read_id()
 
     # run simulation
+    DW_tap_v = Path(__file__).resolve().parent.parent / 'noupload' / 'DW_tap.v'
     t.compile_and_run(
         target='system-verilog',
         simulator='iverilog',
-        ext_libs=[Path('../ignore/DW_tap.v').resolve()],
+        ext_libs=[DW_tap_v],
         parameters=parameters,
         ext_model_file=True
     )
 
     # print ID
+    print('JTAG ID:')
     print('{0:0{1}x}'.format(id_val.value, 8))
 
 if __name__ == '__main__':
