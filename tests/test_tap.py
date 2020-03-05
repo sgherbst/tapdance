ffd701d6a473a06ccf3de82b12a2723e12cf7014
Steven Herbst <sherbst@stanford.edu>
2020-03-04 16:05:16 -0800

    demo of jtag

--- tests/test_tap.py
+++ tests/test_tap.py
@@ -2,9 +2,11 @@ import magma as m
 from pathlib import Path
 from tapdance.jtag_drv import JTAGLowLevelDriver
 
+TOP = Path(__file__).resolve().parent.parent
+
 def test_tap(width=5):
     # declare circuit
-    dut = m.define_from_verilog_file("verilog/dut.v")[0]
+    dut = m.define_from_verilog_file(TOP / 'tests' / 'verilog' / 'dut.v')[0]
 
     # instantiate tester
     t = JTAGLowLevelDriver(dut, width=width)
@@ -16,13 +18,11 @@ def test_tap(width=5):
     id_val = t.read_id()
 
     # run simulation
-    DW_tap_v = Path(__file__).resolve().parent.parent / 'noupload' / 'DW_tap.v'
-    dut_v = Path(__file__).resolve().parent / 'verilog' / 'dut.v'
+    DW_tap_v = TOP / 'noupload' / 'DW_tap.v'
     t.compile_and_run(
         target='system-verilog',
         simulator='iverilog',
-        ext_libs=[DW_tap_v, dut_v],
-        ext_model_file=True
+        ext_libs=[DW_tap_v]
     )
 
     # print ID
